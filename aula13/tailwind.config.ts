75a797cf6b408ecbac25a992ef767251707b8328
victorcosmedev <victorcosme.profissional@gmail.com>
2024-09-27 20:31:59 -0300

    Exemplos práticos de configuração e utilização do tailwind.

--- aula13/tailwind.config.ts
+++ aula13/tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import forms from "@tailwindcss/forms";
 
 const config: Config = {
   content: [
@@ -14,6 +16,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    typography,
+    forms
+  ],
 };
 export default config;
