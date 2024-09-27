e7589acbeced1d5432ceff4ae083bdd2f4f0f58c
Melpscosta <melpscosta@gmail.com>
2024-09-27 20:12:07 -0300

    melico

--- aula13/tailwind.config.ts
+++ aula13/tailwind.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography"
 
+//força configs de tipos para o tailwind
+//content possui um array c 3 itens, com extensoes de aqrq, podendo entrasr nas seguintes pastas para proc atributo class para ver se os componentes podem receber estilos do css
+// /**/ * ta completando os arquivos 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  //podemos alterar o back e fore
   theme: {
     extend: {
       colors: {
@@ -14,6 +19,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    typography
+  ],
 };
 export default config;
